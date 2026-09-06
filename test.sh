6a6cc4b2bb34925d15462944b2f13ed392e33118
Dmitriy Pastukhov-Pugach <pastukhov.da@yahoo.com>
2026-09-06 15:41:40 +0300

    change a,b

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
+##copyright ©Pastukov-Pugach Dmitriy @Pastukhov-Pugach 2026
 #!/bin/bash
 declare -i a #Обьявление целочисленной переменной
-declare -i; declare -i c
-a=28:b; c=$a=$b
-echo $c #Выведет 32
+declare -i b 
+declare -i c
+a=32; b=5; c=$a=$b
+echo $c
 c="Good" #Попытка присваивания строкового значения
 echo $c #Выведет 0
-d=64:e; f=$d+$e #
-echo $f #Выведет 64+1
+d=64; e=1; f=$d+$e
+echo $f
